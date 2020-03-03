7a51bd58daf6b76ba71672afb91608e926a4d53b
Mincky <54250862+mickers13@users.noreply.github.com>
2020-03-03 19:46:55 +0100

    s

---
+++ SQL.py
@@ -0,0 +1,59 @@
+import mysql.connector
+f = open("password.txt", "r")
+password = f.readline()
+print(password)
+def create_database():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=password
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE DATABASE webshop")
+
+#create_database()
+
+def datading():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=password,
+        database="webshop"
+    )
+    print(mydb)
+
+#datading()
+
+def datadinges():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=password,
+        database="webshop"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+#datadinges()
+
+def dataaa():
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=password,
+        database="webshop"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SHOW TABLES")
+
+    for x in mycursor:
+        print(x)
+
+dataaa()
