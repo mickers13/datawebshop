d16a20e84e7dd26921726dca8c7c851c694139ea
michielborghuis <59872213+michielborghuis@users.noreply.github.com>
2020-03-11 18:55:04 +0100

    Update Formatieve Opdracht 2b.py

--- Formatieve Opdracht 2b.py
+++ Formatieve Opdracht 2b.py
@@ -29,11 +29,82 @@ def create_table():
     )
     mycursor = mydb.cursor()
     mycursor.execute("DROP TABLE IF EXISTS products")
-    mycursor.execute("CREATE TABLE products (id VARCHAR(255) PRIMARY KEY, brand VARCHAR(50), color VARCHAR(20), "
-                     "gender VARCHAR(20), category VARCHAR(255), sub_category VARCHAR(255), "
-                     "sub_sub_category VARCHAR(255), name VARCHAR(255), price DECIMAL(10, 2), "
-                     "recommendable BOOLEAN)")
+    mycursor.execute("DROP TABLE IF EXISTS sessions")
+    mycursor.execute("DROP TABLE if EXISTS profiles")
 
+    mycursor.execute("CREATE TABLE profiles ("
+                     "id VARCHAR(255) PRIMARY KEY,"
+                     "segment VARCHAR(255))")
+
+    mycursor.execute("CREATE TABLE buids ("
+                     "buid VARCHAR(255) PRIMARY KEY,"
+                     "profiles_id VARCHAR(255),"
+                     "FOREIGN KEY(profiles_id) REFERENCES profiles(id))")
+
+    mycursor.execute("CREATE TABLE sessions ("
+                     "id VARCHAR(255) PRIMARY KEY, "
+                     "segment VARCHAR(45), "
+                     "has_sale BOOLEAN, "
+                     "sources VARCHAR(45), "
+                     "_events VARCHAR(255), "
+                     "buids_buid VARCHAR(255),"
+                     "FOREIGN KEY(buids_buid) REFERENCES buids(buid))")
+
+    mycursor.execute("CREATE TABLE brands ("
+                     "id INT PRIMARY KEY, "
+                     "brand VARCHAR(45))")
+
+    mycursor.execute("CREATE TABLE categories ("
+                     "id INT PRIMARY KEY, "
+                     "cat VARCHAR(45),"
+                     "subcat VARCHAR(45),"
+                     "subsubcat VARCHAR(45))")
+
+    mycursor.execute("CREATE TABLE genders ("
+                     "id INT PRIMARY KEY, "
+                     "gender VARCHAR(45))")
+
+    mycursor.execute("CREATE TABLE doelgroepen ("
+                     "id INT PRIMARY KEY, "
+                     "doelgroep VARCHAR(45))")
+
+    mycursor.execute("CREATE TABLE products ("
+                     "id INT PRIMARY KEY, "
+                     "name VARCHAR(255), "
+                     "selling_price INT, "
+                     "discount INT, "
+                     "herhaalaankopen BOOLEAN, "
+                     "kleur VARCHAR(255), "
+                     "descr VARCHAR(255), "
+                     "eenheid VARCHAR(255), "
+                     "inhoud VARCHAR(255), "
+                     "leeftijd VARCHAR(255), "
+                     "serie VARCHAR(255), "
+                     "soort VARCHAR(255), "
+                     "sterkte VARCHAR(255), "
+                     "tax VARCHAR(255), "
+                     "weekdeal BOOLEAN, "
+                     "size VARCHAR(255), "
+                     "brand_id INT, "
+                     "cat_id INT, "
+                     "gender_id INT, "
+                     "doelgroep_id INT, "
+                     "FOREIGN KEY(brand_id) REFERENCES brands(id),"
+                     "FOREIGN KEY(cat_id) REFERENCES categories(id),"
+                     "FOREIGN KEY(gender_id) REFERENCES genders(id),"
+                     "FOREIGN KEY(doelgroep_id) REFERENCES doelgroepen(id))")
+
+    mycursor.execute("CREATE TABLE orders ("
+                     "session_id VARCHAR(255),"
+                     "product_id INT,"
+                     "FOREIGN KEY(session_id) REFERENCES sessions(id),"
+                     "FOREIGN KEY(product_id) REFERENCES products(id))")
+
+    mycursor.execute("CREATE TABLE events ("
+                     "session_id VARCHAR(255),"
+                     "product_id INT,"
+                     "FOREIGN KEY(session_id) REFERENCES sessions(id),"
+                     "FOREIGN KEY(product_id) REFERENCES products(id))")
 
 def show_tables():
     mydb = mysql.connect(
