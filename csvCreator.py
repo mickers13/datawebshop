e3d6ec36e1df0294d1a455bd243069644888c9d4
Mincky <54250862+mickers13@users.noreply.github.com>
2020-03-11 19:16:45 +0100

    update voor doelgroep gender en brands

--- csvCreator.py
+++ csvCreator.py
@@ -14,12 +14,16 @@ prof_cur = prof_col.find()
 prod_cur = prod_col.find()
 print( prod_cur[1])
 # werkend maar mist nog een paar functies:
-def csvProducten(data, filename):
-    key_list = ['_id', 'name','gender', 'brand', 'price', 'herhaalaankopen', 'color', 'description', 'properties', 'size']
+def csvProducten(data):
+    key_list = ['_id', 'name','gender','category','subcategory','subsubcategory', 'brand', 'price', 'herhaalaankopen', 'color', 'description', 'properties', 'size']
     key_list_price = ['selling_price', 'discount']
-    key_list_properties = ['eenheid', 'inhoud', 'leeftijd', 'serie', 'soort', 'sterkte', 'tax', 'weekdeal']
+    key_list_properties = ['eenheid', 'inhoud', 'leeftijd', 'serie', 'soort', 'sterkte', 'tax', 'weekdeal', 'doelgroep']
     possible_gender = []
     possible_brands = []
+    possible_doelgroepen = []
+    possible_categories = []
+    possible_subcategories = []
+    possible_subsubcategories = []
     for num, i in enumerate(list(data)):
         value_list = []
         if num == 1 or num == 2:
@@ -33,13 +37,30 @@ def csvProducten(data, filename):
                         possible_brands.append(i[y])
                         value_list.append(possible_brands.index(i[y]))
                 elif y == 'gender':
-                    print(i[y])
                     if i[y] in possible_gender:
                         value_list.append(possible_gender.index(i[y]))
                         print(value_list)
                     else:
                         possible_gender.append(i[y])
                         value_list.append(possible_gender.index(i[y]))
+                elif y == 'category':
+                    if i[y] in possible_categories:
+                        value_list.append(possible_categories.index(i[y]))
+                    else:
+                        possible_categories.append(i[y])
+                        value_list.append(possible_categories.index(i[y]))
+                elif y == 'subcategory':
+                    if i[y] in possible_subcategories:
+                        value_list.append(possible_subcategories.index(i[y]))
+                    else:
+                        possible_subcategories.append(i[y])
+                        value_list.append(possible_subcategories.index(i[y]))
+                elif y == 'subsubcategory':
+                    if i[y] in possible_subsubcategories:
+                        value_list.append(possible_subsubcategories.index(i[y]))
+                    else:
+                        possible_subsubcategories.append(i[y])
+                        value_list.append(possible_subsubcategories.index(i[y]))
                 elif y == 'price':
                     for j in i[y]:
                         if j in key_list_price:
@@ -47,14 +68,20 @@ def csvProducten(data, filename):
                 elif y == 'properties':
                     for j in i[y]:
                         if j in key_list_properties:
-                            value_list.append(i[y][j])
+                            if j == "doelgroep":
+                                possible_doelgroepen.append(i[y][j])
+                                value_list.append(possible_doelgroepen.index(i[y][j]))
+                            else:
+                                value_list.append(i[y][j])
                 else:
                     value_list.append(i[y])
             with open("producten.csv", "a", newline='') as file:
                 inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 inData.writerow(value_list)
+    writecsv("doelgroep.csv", possible_doelgroepen)
     writecsv("gender.csv", possible_gender)
     writecsv("brands.csv", possible_brands)
+    writecsv("categories.csv", possible_categories)
 
 def writecsv(name, possible_watdanook):
     with open(name, "a", newline='') as file:
@@ -65,62 +92,4 @@ def writecsv(name, possible_watdanook):
             inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             inData.writerow(value_list_watdanook)
 
-
-
-
-
-csvProducten(prod_cur, 'producten.csv')
-
-# with open("gender.csv", "a", newline='') as file:
-    #     for i in possible_gender:
-    #         value_list_gender = []
-    #         value_list_gender.append(possible_gender.index(i))
-    #         value_list_gender.append(i)
-    #         inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #         inData.writerow(value_list_gender)
-# ###sessies werkt niet, products wel.
-#
-# def csvSessies(data, filename):
-#     key_list = ['buid', "segment", 'has_sale', 'order', 'preferences', 'sources']
-#     for num, i in enumerate(list(data)):
-#         value_list = []
-#         key_list_order = []
-#         key_list_preferences = []
-#         key_list_sources = []
-#         if num == 1:
-#             for y in key_list:
-#                 try:
-#                     if y == 'buid':
-#                         for j in i[y]:
-#                             value_list.append(i[y][j])
-#                             key_list_order.append(i[y][j])
-#                             key_list_preferences.append(i[y][j])
-#                             key_list_sources.append(i[y][j])
-#                     elif y == 'order':
-#                         for j in i[y][0]:
-#                             for k in j:
-#                                 key_list_order.append(k)
-#                     elif y == 'preferences':
-#                         for j in i[y]:
-#                             key_list_preferences.append(j)
-#                     elif y == 'sources':
-#                         for j in i[y]:
-#                             key_list_preferences.append(j)
-#                     else:
-#                         value_list.append(i[y])
-#                 except:
-#                     value_list.append('NULL')
-#             with open(filename, "a", newline='') as file:
-#                 inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#                 inData.writerow(value_list)
-#             with open('orders.csv', "a", newline='') as a:
-#                 inData = csv.writer(a, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#                 inData.writerow(key_list_order)
-#             with open('preferences.csv', "a", newline='') as b:
-#                 inData = csv.writer(b, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#                 inData.writerow(key_list_preferences)
-#             with open('sources.csv', "a", newline='') as c:
-#                 inData = csv.writer(c, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#                 inData.writerow(key_list_preferences)
-#
-# csvSessies(ses_cur, 'sessions.csv')
+csvProducten(prod_cur)
