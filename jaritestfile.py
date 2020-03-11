9d9a6de3e72325568100fa16e02b3476d40b0917
jariwezerhu <jari.wezer@student.hu.nl>
2020-03-11 19:16:54 +0100

    hierbij mijn oplevering :_(

--- jaritestfile.py
+++ jaritestfile.py
@@ -9,9 +9,14 @@ ses_col = db.sessions # 3
 prof_col = db.profiles # 2
 prod_col = db.products # 1
 
-ses_cur = ses_col.find()
+ses_cur = ses_col.find().limit(30000)
 prof_cur = prof_col.find()
 prod_cur = prod_col.find()
+
+def csvWriter(filename, list):
+    with open(filename, "a", newline='') as file:
+        inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        inData.writerow(list)
 
 
 def csvProducten(data, filename):
@@ -36,55 +41,56 @@ def csvProducten(data, filename):
                         print(i[y])
                 except:
                     value_list.append('NULL')
-            with open(filename, "a", newline='') as file:
-                inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                inData.writerow(value_list)
+            csvWriter(filename, value_list)
 
-csvProducten(prod_cur, 'producten.csv')
+#csvProducten(prod_cur, 'producten.csv')
 
 ###sessies werkt niet, products wel.
 
 def csvSessies(data, filename):
-    key_list = ['buid', "segment", 'has_sale', 'order', 'preferences', 'sources']
+    key_list = ['_id', 'buid', "segment", 'has_sale', 'order', 'events', 'sources']
     for num, i in enumerate(list(data)):
         value_list = []
-        key_list_order = []
-        key_list_preferences = []
-        key_list_sources = []
-        if num == 1:
-            for y in key_list:
-                try:
-                    if y == 'buid':
+        order_list = []
+        source_list = []
+        event_list = []
+        buid_list = []
+        for y in key_list:
+            try:
+                if y == '_id':
+                    value_list.append(i[y])
+                    order_list.append(i[y])
+                    event_list.append(i[y])
+                    source_list.append(i[y])
+                    buid_list.append(i[y])
+                elif y == 'buid':
+                    for j in i[y]:
+                        buid_list.append(j)
+                elif y == 'order':
+                    if i[y] is not None:
                         for j in i[y]:
-                            value_list.append(i[y][j])
-                            key_list_order.append(i[y][j])
-                            key_list_preferences.append(i[y][j])
-                            key_list_sources.append(i[y][j])
-                    elif y == 'order':
-                        for j in i[y][0]:
-                            for k in j:
-                                key_list_order.append(k)
-                    elif y == 'preferences':
-                        for j in i[y]:
-                            key_list_preferences.append(j)
-                    elif y == 'sources':
-                        for j in i[y]:
-                            key_list_preferences.append(j)
+                            for c in i[y][j]:
+                                for m in c.values():
+                                    order_list.append(m)
+                        csvWriter('orders.csv', order_list)
                     else:
-                        value_list.append(i[y])
-                except:
-                    value_list.append('NULL')
-            with open(filename, "a", newline='') as file:
-                inData = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                inData.writerow(value_list)
-            with open('orders.csv', "a", newline='') as a:
-                inData = csv.writer(a, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                inData.writerow(key_list_order)
-            with open('preferences.csv', "a", newline='') as b:
-                inData = csv.writer(b, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                inData.writerow(key_list_preferences)
-            with open('sources.csv', "a", newline='') as c:
-                inData = csv.writer(c, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                inData.writerow(key_list_preferences)
+                        pass
+                elif y == 'events':
+                    for j in i[y]:
+                        for c in j:
+                            if c == 'product' and j[c] is not None:
+                                event_list.append(j[c])
+                    if len(event_list) > 1:
+                        csvWriter('events.csv', event_list)
+                elif y == 'sources':
+                    for j in i[y]:
+                        source_list.append(j['full_url'])
+                    csvWriter('sources.csv', source_list)
+                else:
+                    value_list.append(i[y])
+            except:
+                pass
+        csvWriter(filename, value_list)
+        csvWriter('buid.csv', buid_list)
 
 csvSessies(ses_cur, 'sessions.csv')
